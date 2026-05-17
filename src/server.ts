0c40465e7251cc9f91ee00b0a1ff4674a02a9e58
st-shourov12 <shourovsc38@gmail.com>
2026-05-18 00:53:39 +0600

    add post method

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
-import express from "express"
-const app = express();
+import express, { type Application, type Request, type Response } from "express"
+const app: Application = express();
 const port = 5000;
 
-app.get("/", (req, res)=> {
-    res.send("hello shourov")
+app.use(express.json());
+app.use(express.text())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req : Request, res : Response)=> {
+    // res.send("Express Server")
+    res.status(200).json({
+        message : "Express server",
+        "author" : "Shourov"
+    })
+})
+
+app.post("/", async(req : Request, res : Response) =>{
+    console.log(req.body);
 })
 
 app.listen(port, ()=>{
