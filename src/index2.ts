132947a216ec1f3c7280afa57f5be00ea9d7bb53
st-shourov12 <shourovsc38@gmail.com>
2026-05-17 23:29:25 +0600

    first commit

---
+++ src/index2.ts
@@ -0,0 +1,11 @@
+/** 
+ * npm init --y
+ * npm i -D typescript
+ * npx tsc --init
+ * root, file directory uncomment on tsconfig.json
+ * module esnext, types ["node"], comment jsx on tsconfig.json
+ * type module on package.json
+ * npm install express --save
+ * npm i --save-dev @types/express
+ * npm i -D tsx
+ */
